3530abdf8708c687d6c439f5fd31b18866f07ee6
Santhosh kumar <santhoshmahii25@gmail.com>
2024-11-06 16:20:38 +0530

    Second commit complete react code

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World!! From the Vercel deploy React application</h1>
+    </div>
+  );
+}
+
+export default App;
